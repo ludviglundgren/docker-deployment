8b35bf47535641d1d383cf0ac674f81bdf89e338
Ludvig Lundgren <hello@ludviglundgren.se>
2018-04-26 18:35:03 +0200

    Update index.js

--- swarm/basic-traefik-and-docker-swarm-setup/app/index.js
+++ swarm/basic-traefik-and-docker-swarm-setup/app/index.js
@@ -9,7 +9,7 @@ app.use(cors({
 		methods: '*'
 }));
 
-app.get('/', (req, res) => res.json('Hello World! Build with Buddy Ci!!'));
+app.get('/', (req, res) => res.json('Hello World! Built with Buddy.works ci/cd!!'));
 
 app.listen(3000, () => {
 		console.log("Hello world!")
