b9572ded9272535b3cc6dc7c08dad996d9587c2c
Ludvig Lundgren <hello@ludviglundgren.se>
2018-04-30 19:18:28 +0200

    new commit

--- swarm/basic-traefik-and-docker-swarm-setup/app/index.js
+++ swarm/basic-traefik-and-docker-swarm-setup/app/index.js
@@ -9,7 +9,7 @@ app.use(cors({
 		methods: '*'
 }));
 
-app.get('/', (req, res) => res.json('Hello World!!! '));
+app.get('/', (req, res) => res.json('Hello World!!!  New update!'));
 
 app.listen(3000, () => {
 		console.log("Hello world!")
